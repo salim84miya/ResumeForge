e6fb4d695527e6c8f52b5042ae1e1ef2819c434e
salim84miya <salimsaudagar84@gmai.com>
2026-04-22 17:03:53 +0530

    remove other controller methods from profile controller

--- src/main/java/com/springai/resumax/profile/controller/ProfileController.java
+++ src/main/java/com/springai/resumax/profile/controller/ProfileController.java
@@ -10,7 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,10 +17,6 @@ public class ProfileController {
 
 
     private final UserProfileService profileService;
-    private final ProjectService projectService;
-    private final ExperienceService experienceService;
-    private final EducationService educationService;
-    private final UserSkillService skillService;
 
     @PostMapping("/profile")
     public ResponseEntity<?> saveProfile(@RequestBody UserProfileInsertDto profileInsertDto){
@@ -55,41 +50,5 @@ public class ProfileController {
 
         return ResponseHandler.builder(response,null,HttpStatus.OK,LocalDateTime.now());
     }
-
-
-
-    @PostMapping("/skill")
-    public ResponseEntity<List<UserSkill>> saveSkills(@RequestBody InsertSkillDto dto){
-
-        var response = skillService.saveMultipleSkill(dto);
-
-        return ResponseEntity.ok(response);
-    }
-
-
-    @PostMapping("/project")
-    public ResponseEntity<UserProject> saveProject(@RequestBody UserProjectInsertDto projectInsertDto){
-
-        var response = projectService.saveProject(projectInsertDto);
-
-        return ResponseEntity.ok(response);
-    }
-
-    @PostMapping("/experience")
-    public ResponseEntity<UserExperience> saveExperience(@RequestBody UserExperienceInsertDto insertDto){
-
-        var response = experienceService.saveExperience(insertDto);
-
-        return ResponseEntity.ok(response);
-    }
-
-    @PostMapping("/education")
-    public ResponseEntity<UserEducation> saveEducation(@RequestBody EducationInsertDto educationInsertDto){
-
-        var response = educationService.saveEducation(educationInsertDto);
-
-        return ResponseEntity.ok(response);
-    }
-
 
 }
