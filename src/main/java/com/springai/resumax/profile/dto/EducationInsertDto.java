a29da7b1e8ea6924b3fd36263bf3e349538924f8
salim84miya <salimsaudagar84@gmail.com>
2026-04-27 12:27:36 +0530

    added validation to education insert dto

--- src/main/java/com/springai/resumax/profile/dto/EducationInsertDto.java
+++ src/main/java/com/springai/resumax/profile/dto/EducationInsertDto.java
@@ -1,11 +1,13 @@
 package com.springai.resumax.profile.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 
 @Data
 public class EducationInsertDto {
 
+    @NotBlank
     private String qualification; // company / org
     private String timeline;
     private String grade;
